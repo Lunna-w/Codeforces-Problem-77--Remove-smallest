80c834ab70ea4a9455255820a884449928657346
Daria-Iulia Berce <daria.berce1@gmail.com>
2023-08-04 11:37:50 +0100

    Create remove_smallest.py

---
+++ remove_smallest.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    for i in range(1, n):
+        if a[i] - a[i-1] > 1:
+            print("NO")
+            break
+    else:
+        print("YES")
